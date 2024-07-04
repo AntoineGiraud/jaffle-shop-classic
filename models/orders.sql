48751da04bcc5a8d2d86753e4718e8e17cf30704
AntoineGiraud <antoine.giraud@outlook.com>
2024-07-04 22:30:14 +0200

    feat: ajustements indentation SQL & src

--- models/orders.sql
+++ models/orders.sql
@@ -1,15 +1,10 @@
 {% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
 
 with orders as (
-
     select * from {{ ref('stg_orders') }}
-
 ),
-
 payments as (
-
     select * from {{ ref('stg_payments') }}
-
 ),
 
 order_payments as (
@@ -24,33 +19,25 @@ order_payments as (
         sum(amount) as total_amount
 
     from payments
-
-    group by order_id
-
-),
-
-final as (
-
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        orders.status,
-
-        {% for payment_method in payment_methods -%}
-
-        order_payments.{{ payment_method }}_amount,
-
-        {% endfor -%}
-
-        order_payments.total_amount as amount
-
-    from orders
-
-
-    left join order_payments
-        on orders.order_id = order_payments.order_id
+    group by 1
 
 )
 
-select * from final
+select
+    orders.order_id,
+    orders.customer_id,
+    orders.order_date,
+    orders.status,
+
+    {% for payment_method in payment_methods -%}
+
+    order_payments.{{ payment_method }}_amount,
+
+    {% endfor -%}
+
+    order_payments.total_amount as amount
+
+from orders
+    left join order_payments
+        using(order_id)
+        {# on orders.order_id = order_payments.order_id #}
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,25 +1,16 @@
 with source as (
-    
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_payments') }}
-
-),
-
-renamed as (
-
-    select
-        id as payment_id,
-        order_id,
-        payment_method,
-
-        -- `amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
-
-    from source
-
+    select * from {{ source('raw_stripe', 'raw_payment') }}
 )
 
-select * from renamed
+select
+    id as payment_id,
+    orderid as order_id,
+
+    paymentmethod as payment_method,
+    status,
+    created,
+
+    -- `amount` is currently stored in cents, so we convert it to dollars
+    amount / 100 as amount
+
+from source
